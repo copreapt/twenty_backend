3fc84abd8e3210366902327ab5aeaf0dbee68310
Oprea Catalin <copreapt@gmail.com>
2023-11-02 20:19:20 +0200

    auth complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require('../errors');
+const jwt = require("jsonwebtoken");
+
 
 
 const register = async (req, res) => {
@@ -10,10 +12,11 @@ const register = async (req, res) => {
   if(emailAddressExists){
     throw new CustomError.BadRequestError('Email already exists');
   }
-
   const user = await User.create({email, fullName, username, password});
-  token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({msg:'Success! Please check your email to verify the account', user:user});
+  const token = jwt.sign({user}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+   });
+  res.status(StatusCodes.CREATED).json({msg:'Success! Please check your email to verify the account', token});
 };
 
 const verifyEmail = async (req, res) => {
@@ -21,12 +24,30 @@ const verifyEmail = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const user = req.body
-  res.status(StatusCodes.OK).json({user})
+  const {email, password} = req.body;
+if(!email || !password){
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({email});
+if(!user){
+  throw new CustomError.UnauthenticatedError('Invalid Credentials');
+}
+  const isPasswordCorrect = await user.comparePassword(password);
+if(!isPasswordCorrect){
+  throw new CustomError.UnauthenticatedError('Invalid Credentials');
+}
+  const token =  jwt.sign({user}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  res.status(StatusCodes.OK).json({msg:`Welcome ${user.username}`,token});
 };
 
 const logout = async (req, res) => {
-  res.send("logout user");
+  res.cookie('token', 'logout', {
+    httpOnly:true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({msg: "user logged out"});
 };
 
 module.exports = { register, verifyEmail, login, logout };
